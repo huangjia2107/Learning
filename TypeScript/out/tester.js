8edb89cba8170af1adc5b4577488db3fffca0efe
huangjia <654664168@qq.com>
2020-06-28 09:20:32 +0800

    add .gitignore and utils.ts

--- TypeScript/out/tester.js
+++
@@ -1,5 +0,0 @@
-let p = Promise.resolve(8);
-let p1 = p.then((value) => Error("fff"));
-p1.then((value) => console.log(value, p1));
-p1.catch((r) => console.log(r));
-//# sourceMappingURL=tester.js.map
